85b50646b21faf168378ddd7fb02e59a689ad08e
stalygin <gintare.stalygaite@tieto.com>
2021-02-11 08:12:30 +0200

    *init *swagger

---
+++ Ondato_WebApi/Controllers/ObjectsCrudController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ondato_WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ObjectsCrudController : ControllerBase
+    {
+
+
+        private readonly ILogger<ObjectsCrudController> _logger;
+
+        public ObjectsCrudController(ILogger<ObjectsCrudController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
